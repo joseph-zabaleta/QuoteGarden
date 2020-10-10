ae1db81fc9640d622b1d61698453a835ac44f618
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-10-09 22:27:02 -0700

    Changed middleware to be testable code

---
+++ __tests__/api/middleware/cache.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+const { isCached, isCachedAll } = require('../../../api/middlewares/cache.js');
+
+describe('Testing Cache Middleware Functionaility: ', () => {
+
+    it('Should work', () => {
+
+        expect(1).toEqual(1);
+
+    });
+
+});
--- api/middlewares/cache.js
+++ api/middlewares/cache.js
@@ -1,6 +1,8 @@
 const redisClient = require('../configs/redisConfig');
+const { promisify } = require('util');
+const getAsyncClient = promisify(redisClient.get).bind(redisClient);
 
-exports.isCached = (req, res, next) => {
+exports.isCached = async (req, res, next) => {
 
     const { authorName, searchQuery } = req.params;
     const { page = 1, limit = 10 } = req.query;
@@ -8,39 +10,37 @@ exports.isCached = (req, res, next) => {
     const authorId = authorName + "-" + page + "-" + limit;
     const searchId = searchQuery + "-" + page + "-" + limit;
 
-    //Check in redis
-    redisClient.get(authorName ? authorId : searchId, (err, reply) => {
-        if (err) {
-            console.log(err);
-        }
+    const reply = await getAsyncClient(authorName ? authorId : searchId);
 
-        if (reply) {
-            const response = JSON.parse(reply)
-            return res.status(200).json(response);
-        } else {
-            next();
-        }
-    });
+    if (reply) {
+
+        const response = JSON.parse(reply);
+        return res.status(200).json(response);
+
+    } else {
+
+        next();
+
+    };
+
 }
 
-exports.isCachedAll = (req, res, next) => {
+exports.isCachedAll = async (req, res, next) => {
 
     const { page = 1, limit = 10 } = req.query;
 
     const allId = "quote" + "-" + page + "-" + limit;
 
-    redisClient.get(allId, (err, reply) => {
-        if (err) {
-            console.log(err);
-        }
+    const reply = await getAsyncClient(allId);
 
-        if (reply) {
-            const response = JSON.parse(reply);
-            return res.status(200).json(response);
-        } else {
-            next();
-        }
+    if (reply) {
 
-    });
+        const response = JSON.parse(reply);
+        return res.status(200).json(response);
 
+    } else {
+
+        next();
+
+    };
 }
